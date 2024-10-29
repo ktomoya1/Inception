9d3c1b758af8c89eb02963542431ed9d9bf5329a
tom <134240277+ktomoya1@users.noreply.github.com>
2024-10-29 13:58:13 +0900

    fix: user name

--- srcs/requirements/mariadb/init_db.sh
+++ srcs/requirements/mariadb/init_db.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "ALTER USER '${MYSQL_ROOT_HOST}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" > /docker-entrypoint-initdb.d/init.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" > /docker-entrypoint-initdb.d/init.sql
 
 echo "FLUSH PRIVILEGES;" >> /docker-entrypoint-initdb.d/init.sql
 
 echo "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;" >> /docker-entrypoint-initdb.d/init.sql
 
-echo "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> /docker-entrypoint-initdb.d/init.sql
+echo "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> /docker-entrypoint-initdb.d/init.sql
 
-echo "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';" >> /docker-entrypoint-initdb.d/init.sql
+echo "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'localhost';" >> /docker-entrypoint-initdb.d/init.sql
 
 echo "FLUSH PRIVILEGES;" >> /docker-entrypoint-initdb.d/init.sql
 
